c44ea631925ed34523458da880c2276467fa2981
haravetskyy <yuraharavetskyy@gmail.com>
2025-04-20 21:14:24 +0200

    feat(ui): Add `shadcn/ui` library

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 'use server';
 
 const Home = async () => {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_URL!);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recipes?id=52772`);
   const text = await response.text();
 
   return <p>Server says: {text}</p>;
