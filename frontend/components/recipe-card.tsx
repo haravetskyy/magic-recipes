be578cf2b436f3ae838c101e232131e5c12a2929
haravetskyy <yuraharavetskyy@gmail.com>
2025-04-21 22:49:59 +0200

    feat(recipes): Add `recipe-section` and `recipe-card` with responsive layout

---
+++ frontend/components/recipe-card.tsx
@@ -0,0 +1,36 @@
+import { Recipe } from '@/types/recipe';
+import { CookingPot } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Badge } from './ui/badge';
+import { Button } from './ui/button';
+import { Card, CardTitle } from './ui/card';
+
+const RecipeCard = ({ id, name, category, area, thumbnail }: Recipe) => {
+  return (
+    <Card>
+      <CardTitle className="text-2xl">{name}</CardTitle>
+
+      <Image
+        src={thumbnail}
+        alt={`Image for ${name}`}
+        width={300}
+        height={300}
+        className="rounded-lg w-full"
+      />
+
+      <div className="flex flex-row gap-2">
+        <Badge variant="secondary">{category}</Badge>
+        <Badge variant="secondary">{area}</Badge>
+      </div>
+
+      <Link href={`/recipe&id=${id}`}>
+        <Button className="cursor-pointer w-full px-12 ">
+          See complete recipe <CookingPot />
+        </Button>
+      </Link>
+    </Card>
+  );
+};
+
+export default RecipeCard;
---
+++ frontend/components/recipe-section.tsx
@@ -0,0 +1,39 @@
+import { RecipesResponse } from '@/types/recipe';
+import RecipeCard from './recipe-card';
+
+async function fetchAllRecipes(): Promise<RecipesResponse> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recipes/getRecipes`, {
+    cache: 'no-store',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch all recipes');
+  }
+
+  const data: RecipesResponse = await response.json();
+  return data || { recipes: [] };
+}
+
+let recipes: RecipesResponse['recipes'] = [];
+
+try {
+  const recipesData = await fetchAllRecipes();
+  recipes = recipesData.recipes || [];
+} catch (error) {
+  console.error('Error fetching recipes:', error);
+  recipes = [];
+}
+
+const RecipeSection = async () => {
+  return (
+    <section className="md:p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+      {recipes.length > 0 ? (
+        recipes.map(recipe => <RecipeCard key={recipe.id} {...recipe} />)
+      ) : (
+        <p>No recipes found.</p>
+      )}
+    </section>
+  );
+};
+
+export default RecipeSection;
