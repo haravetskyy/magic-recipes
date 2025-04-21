f6411fd74532972bdcf1b38edbeece95d7ad453b
haravetskyy <yuraharavetskyy@gmail.com>
2025-04-21 22:48:58 +0200

    chore(filters): Update `filter-section` to be shorter and have filters opened by default

--- frontend/components/filter-section.tsx
+++ frontend/components/filter-section.tsx
@@ -2,10 +2,6 @@ import { FilterResponse, FilterValue } from '@/types/recipe';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
 import { Badge } from './ui/badge';
 
-const filterSectionContent = {
-  mainHeading: 'What to cook?',
-};
-
 async function fetchFilterValues(
   filterType: 'ingredient' | 'area' | 'category',
 ): Promise<FilterValue[]> {
@@ -22,29 +18,27 @@ async function fetchFilterValues(
   return data.values || [];
 }
 
-let ingredientFilters: FilterValue[] = [];
 let areaFilters: FilterValue[] = [];
 let categoryFilters: FilterValue[] = [];
 
 try {
-  [ingredientFilters, areaFilters, categoryFilters] = await Promise.all([
-    fetchFilterValues('ingredient'),
+  [areaFilters, categoryFilters] = await Promise.all([
     fetchFilterValues('area'),
     fetchFilterValues('category'),
   ]);
 } catch (error) {
   console.error('Error fetching filters:', error);
-  ingredientFilters = [];
   areaFilters = [];
   categoryFilters = [];
 }
 
 const FilterSection = async () => {
   return (
-    <section className="flex flex-col gap-4 md:px-4 items-center">
-      <h2 className="font-semibold text-4xl">{filterSectionContent.mainHeading}</h2>
-      <Accordion type="multiple" className="w-full">
-        <AccordionItem value="item-1">
+    <section className="flex flex-col gap-4 md:p-4 items-center w-full">
+      <h2 className="font-semibold text-4xl">What to cook?</h2>
+
+      <Accordion type="multiple" className="w-full" defaultValue={['areas', 'categories']}>
+        <AccordionItem value="areas">
           <AccordionTrigger className="w-full">Filter by area</AccordionTrigger>
           <AccordionContent className="flex flex-row flex-wrap gap-2">
             {areaFilters.map(area => (
@@ -54,26 +48,13 @@ const FilterSection = async () => {
             ))}
           </AccordionContent>
         </AccordionItem>
-      </Accordion>
-      <Accordion type="multiple" className="w-full">
-        <AccordionItem value="item-1">
+
+        <AccordionItem value="categories">
           <AccordionTrigger className="w-full">Filter by category</AccordionTrigger>
           <AccordionContent className="flex flex-row flex-wrap gap-2">
             {categoryFilters.map(category => (
               <Badge variant="outline" key={category.id}>
                 {category.name}
-              </Badge>
-            ))}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="multiple" className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger className="w-full">Filter by ingredient</AccordionTrigger>
-          <AccordionContent className="flex flex-row flex-wrap gap-2">
-            {ingredientFilters.map(ingredient => (
-              <Badge variant="outline" key={ingredient.id}>
-                {ingredient.name}
               </Badge>
             ))}
           </AccordionContent>
