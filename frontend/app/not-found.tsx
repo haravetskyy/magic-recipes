3c59b26120a1ea9bbe110f68ed0f740986992895
haravetskyy <yuraharavetskyy@gmail.com>
2025-04-21 22:47:58 +0200

    feat(404): Add custom `not-found` page

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const NotFound = () => (
+  <div className="flex min-h-screen flex-col items-center justify-center bg-background text-foreground">
+    <h1 className="text-4xl font-bold">Page Not Found</h1>
+    <p className="mt-4 text-lg">Sorry, the page you’re looking for doesn’t exist.</p>
+    <Button asChild className="mt-6">
+      <Link href="/">Go to Main Page</Link>
+    </Button>
+  </div>
+);
+
+export default NotFound;
