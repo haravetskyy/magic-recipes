2f0acf117b8b034b2434645e4e27a57795b584fa
haravetskyy <yuraharavetskyy@gmail.com>
2025-04-22 02:12:16 +0200

    feat(ui): Add onHover effect on logo in `nav` component

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -1,14 +1,17 @@
 import { Github, Sparkles } from 'lucide-react';
 import Link from 'next/link';
 import ThemeToggle from './theme-toggle';
+import { Button } from './ui/button';
 
 const Nav = () => {
   return (
     <section className="rounded-lg flex flex-row md:px-6 gap-4 items-center justify-between">
-      <Link href={'/'} className="flex flex-row gap-2 items-center">
-        <Sparkles />
-        <h2 className="font-semibold text-lg">Magic Recipes</h2>
-      </Link>
+      <Button variant="link">
+        <Link href={'/'} className="flex flex-row gap-2 items-center">
+          <Sparkles />
+          <h2 className="font-semibold text-lg">Magic Recipes</h2>
+        </Link>
+      </Button>
 
       <div className="flex flex-row gap-4 items-center">
         <Link href={'https://github.com/haravetskyy/magic-recipe'}>
