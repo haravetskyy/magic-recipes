bf5b41d75220b884d4b2ff148aa01c2db0af9a09
haravetskyy <yuraharavetskyy@gmail.com>
2025-04-22 02:15:47 +0200

    feat(not-found): Add fancy icon to `not-found` page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,14 +1,19 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { Home } from 'lucide-react';
 import Link from 'next/link';
 
 const NotFound = () => (
   <div className="flex h-full flex-col items-center justify-center bg-background text-foreground">
     <h1 className="text-4xl font-bold">Page Not Found</h1>
+
     <p className="mt-4 text-lg">Sorry, the page you’re looking for doesn’t exist.</p>
+
     <Button asChild className="mt-6">
-      <Link href="/">Go to Main Page</Link>
+      <Link href="/">
+        Go to Main Page <Home />
+      </Link>
     </Button>
   </div>
 );
